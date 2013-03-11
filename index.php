afb07e4e46dbb34fe98b7fb2299e743545b4272d
Hussain Jawadwala <hussain@jawadwala.com>
2013-03-11 12:23:22 +0530

    check mail

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form>
+            <label>
+                Name:
+            </label>
+            <input type="text" name="name" value="">
+            <br>
+            <label>
+                email:
+            </label>
+            <input  type="text" name=""
+        </form>
+        
+    </body>
+</html>
