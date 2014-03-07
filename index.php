9a8b9bf6b2ab09ecd8e305a4606130d267e25cfa
Hussain Jawadwala <hussain@jawadwala.com>
2014-03-07 23:11:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -44,6 +44,7 @@
         </tr>
       </table>
     </form>
+    <!-- comment -->
     <script type="text/javascript">
 var sprytextfield1 = new Spry.Widget.ValidationTextField("sprytextfield1");
 var sprytextfield2 = new Spry.Widget.ValidationTextField("sprytextfield2", "email");
